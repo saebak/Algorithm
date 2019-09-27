50969713149dd3cf4541e9043d2d60728a225593
Park Saemi <45276842+saebak@users.noreply.github.com>
2019-09-27 09:43:08 +0900

    algorithm test08
    jungol algorithm test 2604
    same bowl height+ 5
    other bowl height+10

--- jungol/2604.java
+++ jungol/2604.java
@@ -11,15 +11,15 @@ public class bowlHeight {
 	
 	public static int bowl(String s) {
 		
-		int height = 0;
+		int height = 0;	//최종 높이
 			
-			if(s.charAt(0)=='(' ||s.charAt(0)==')') {
-				height +=10;
-				for(int i=1; i<s.length(); i++) {
-					if(s.charAt(i) == s.charAt(i-1)) {
-						height += 5;
+			if(s.charAt(0)=='(' ||s.charAt(0)==')') {	//단어가 ( 나 )이면 그릇높이 계산
+				height +=10;	//첫 그릇높이는 무조건 + 10
+				for(int i=1; i<s.length(); i++) {	//다음그릇부터
+					if(s.charAt(i) == s.charAt(i-1)) {	//전그릇과 똑같은 모양이라면
+						height += 5;	//+5
 					}else{
-						height += 10;
+						height += 10;	//전 그릇과 다른모양이라면 +10
 					}
 				}
 			}
