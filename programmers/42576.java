c04d2d429a6b4b80b3ba4242651952188486d019
Park Saemi <45276842+saebak@users.noreply.github.com>
2019-09-29 10:42:56 +0900

    algorithm test09
    programmers level1 algorithm test 42576
    Find an unfulfilled player

---
+++ programmers/42576.java
@@ -0,0 +1,34 @@
+package com.coding.test;
+
+public class NOgoal {
+	public static void main(String[] args) {
+		
+		String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] completion = {"josipa", "filipa", "marina", "nikola"};
+		
+		goal(participant, completion);
+	}
+	
+	public static void goal(String[] participant, String[] completion) {
+		
+		for(int i=0; i<participant.length; i++) {
+			
+			String find = " ";
+			
+			for(int j=0; j<completion.length; j++) {
+				if(completion[j] == participant[i]) {
+					find = "goal";
+					break;
+				}else if(completion[j] != participant[i]) {
+					find = participant[i];
+				}
+			}
+			
+			if(find != "goal") {
+				System.out.println(find);
+				break;
+			}
+		}
+		
+	}
+}
